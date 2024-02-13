b332da0d5000feebfc2d9a3cbc525198e1987dcd
최종규 <153177760+jonggu12@users.noreply.github.com>
2024-02-13 09:49:53 +0900

    Update install.bat

--- Env/install.bat
+++ Env/install.bat
@@ -1,5 +1,5 @@
-conda create --name envFirst3 python=3.10
-conda activate envFirst3
+conda create --name envFirstMini python=3.10
+conda activate envFirstMini
 curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe -o miniconda.exe
 
 pip install -r requirements.txt
