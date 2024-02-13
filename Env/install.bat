22aafb241b8fdf1ce21d7c549cf558bdb2bfd04a
jonggu12 <whdrb9542@gmail.com>
2024-02-13 09:48:13 +0900

    1

---
+++ Env/install.bat
@@ -0,0 +1,5 @@
+conda create --name envFirst3 python=3.10
+conda activate envFirst3
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe -o miniconda.exe
+
+pip install -r requirements.txt
